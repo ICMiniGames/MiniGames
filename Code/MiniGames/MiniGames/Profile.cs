db583aec29ffb58c5ee841dbf8044c5e879264a4
Cyril GOLDENSCHUE <Cyril.GOLDENSCHUE@cpnv.ch>
2018-11-28 10:29:33 +0100

    changement de l'emplacement de la solution du code

--- Code/MiniGames/MiniGames/Profile.cs
+++ Code/MiniGames/MiniGames/Profile.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-public class Class1
+namespace MiniGames
 {
-	public Class1()
-	{
-	}
+    public partial class Profile : Form
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+    }
 }
